f39214dd03821d81ec2a5e026ab08a8967816ff3
German Zabludin <g.zabludin@gmail.com>
2026-01-13 22:13:36 +0300

    --delete KIND

--- kind/start-kind-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "=== Starting kind cluster containers ==="
-
-docker ps -a --filter "name=ats-" --format "{{.ID}}" | xargs -r docker start
-
-echo "=== kind cluster started ==="
--- kind/stop-kind-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "=== Stopping kind cluster containers ==="
-
-docker ps --filter "name=ats-" --format "{{.ID}}" | xargs -r docker stop
-
-echo "=== kind cluster stopped ==="
